c8dd36ad24686ddf8b7110b1a9f90d39e8e3aac4
Kraust <secretdragoon@gmail.com>
2024-09-04 21:35:22 -0400

    Updated process.py

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import argparse
 import json
 from datetime import datetime
 from pathlib import Path
+import sys
 
 
 def output(out, data):
@@ -14,61 +15,66 @@ def output(out, data):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", type=str)
-    parser.add_argument("-o", "--output", type=str, default="/dev/stdout")
+    parser.add_argument("-o", "--output", type=str, default=None)
     parser.add_argument("-f", "--field", type=str, default="Dilithium Ore")
     parser.add_argument("-p", "--per-day", type=int, default=3)
     args = parser.parse_args()
 
-    with open(args.output, "w") as out:
-        output(
-            out,
-            [
-                "Start Date",
-                "End Date",
-                "Duration",
-                args.field,
-                "PS",
-                "PH",
-                "PD",
-                "PW",
-                "PM",
-            ],
-        )
-        for file in Path(args.directory).glob("*.json"):
-            with open(file) as fd:
-                data = json.load(fd)
-                output(
-                    out,
-                    [
-                        datetime.fromtimestamp(data["start"]).isoformat(),
-                        datetime.fromtimestamp(data["end"]).isoformat(),
-                        str(data["duration"]),
-                        str(data["rewards"].get(args.field)),
-                        str(data["rewards"].get(args.field) / data["duration"]),
-                        str(data["rewards"].get(args.field) / data["duration"] * 3600),
-                        str(
-                            data["rewards"].get(args.field)
-                            / data["duration"]
-                            * 3600
-                            * args.per_day
-                        ),
-                        str(
-                            data["rewards"].get(args.field)
-                            / data["duration"]
-                            * 3600
-                            * args.per_day
-                            * 7
-                        ),
-                        str(
-                            data["rewards"].get(args.field)
-                            / data["duration"]
-                            * 3600
-                            * args.per_day
-                            * 7
-                            * 4
-                        ),
-                    ],
-                )
+
+    if args.output:
+        out = open(args.output, "w")
+    else:
+        out = sys.stdout
+
+    output(
+        out,
+        [
+            "Start Date",
+            "End Date",
+            "Duration",
+            args.field,
+            "PS",
+            "PH",
+            "PD",
+            "PW",
+            "PM",
+        ],
+    )
+    for file in Path(args.directory).glob("*.json"):
+        with open(file) as fd:
+            data = json.load(fd)
+            output(
+                out,
+                [
+                    datetime.fromtimestamp(data["start"]).isoformat(),
+                    datetime.fromtimestamp(data["end"]).isoformat(),
+                    str(data["duration"]),
+                    str(data["rewards"].get(args.field)),
+                    str(data["rewards"].get(args.field) / data["duration"]),
+                    str(data["rewards"].get(args.field) / data["duration"] * 3600),
+                    str(
+                        data["rewards"].get(args.field)
+                        / data["duration"]
+                        * 3600
+                        * args.per_day
+                    ),
+                    str(
+                        data["rewards"].get(args.field)
+                        / data["duration"]
+                        * 3600
+                        * args.per_day
+                        * 7
+                    ),
+                    str(
+                        data["rewards"].get(args.field)
+                        / data["duration"]
+                        * 3600
+                        * args.per_day
+                        * 7
+                        * 4
+                    ),
+                ],
+            )
 
 
 if __name__ == "__main__":
